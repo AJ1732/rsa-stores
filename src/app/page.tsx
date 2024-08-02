18c49560c1a843de64a8117f4ce774310e2bd609
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 00:15:13 +0100

    chore: make nav responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
 export default async function Home() {
   return (
-    <main className="min-h-[calc(100dvh-100px)] text-neutral-950">
-      <section>Body</section>
+    <main className="mt-24 min-h-[calc(100dvh-100px)] text-neutral-950">
+      <section></section>
+      <h2>Body</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione ullam
+        nobis, reiciendis laboriosam necessitatibus sit assumenda, fugit
+        deserunt saepe architecto maxime placeat quasi beatae aliquid dolorem
+        vitae? Repudiandae, illo nisi? Odio velit et officia voluptatum totam
+        aperiam non libero ipsa sit nobis qui corrupti numquam, labore excepturi
+        praesentium architecto. Atque molestias quaerat nam nesciunt iusto
+        reprehenderit necessitatibus quis quae. Odit molestiae sit voluptatem
+        alias molestias labore necessitatibus voluptates. Dolore consequatur
+        voluptates, dolores esse voluptatem tempora illum et qui. Officiis
+        incidunt consectetur consequuntur ad quam ipsa maiores exercitationem
+        architecto. Unde eum possimus porro rem earum debitis fuga, est pariatur
+        illum blanditiis quibusdam laborum animi! Eligendi blanditiis tempora,
+        similique quidem distinctio debitis ratione quisquam at esse in suscipit
+        ipsa facere iusto minima dicta possimus voluptatum iure numquam
+        consequuntur voluptatem. Similique aperiam iusto velit voluptate. Fugit,
+        aliquid eum provident fuga recusandae deserunt corporis distinctio eius
+        voluptas voluptate similique accusantium est pariatur sint neque culpa
+        repudiandae illum vel. Ullam unde laudantium iusto, similique incidunt
+        earum dolorum nostrum debitis sint? Est aspernatur necessitatibus
+        molestias ratione! Tenetur ut laudantium libero dicta eaque aperiam
+        doloribus rerum officia quisquam illo reiciendis at cum suscipit nostrum
+        laborum optio iure totam, quibusdam ipsam sint minus quasi molestiae.
+        Similique, debitis dolore.
+      </p>
     </main>
   );
 }
--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,23 +1,44 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 import { InputField } from "@/components";
-import { CircleUserRound, SearchIcon, ShoppingCart } from "lucide-react";
+import {
+  AlignLeft,
+  CircleUserRound,
+  SearchIcon,
+  ShoppingCart,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const navlinks: NavLinks[] = [
+  {
+    name: "shop",
+    link: "/shop",
+  },
+];
 
 const Navbar = () => {
-  const navlinks: NavLinks[] = [
-    {
-      name: "shop",
-      link: "/shop",
-    },
-  ];
+  const [openNav, setOpenNav] = useState(false);
+
   return (
-    <header className="">
-      <nav className="flex items-center justify-between py-6">
+    <header className="content-grid fixed w-full bg-white">
+      <nav className="flex items-center justify-between gap-4 py-3 md:py-4 lg:py-6">
         <div className="flex items-center justify-between gap-8">
+          {/* MOBILE TOGGLE */}
+          <button
+            className="active:scale-95 lg:hidden"
+            onClick={() => setOpenNav((prev) => !prev)}
+          >
+            <AlignLeft className="size-8" />
+          </button>
+
+          {/* LOGO */}
           <Link href={`/`}>
             <h1 className="mb-2 font-integral text-4xl">RSA</h1>
           </Link>
 
-          <ul className="flex items-center justify-center gap-6">
+          {/* DESKTOP NAVIGATION */}
+          <ul className="flex items-center justify-center gap-6 max-lg:hidden">
             {navlinks.map(({ name, link }) => (
               <li key={name} className="text-xl">
                 <Link href={link} className="block">
@@ -31,17 +52,45 @@ const Navbar = () => {
         <InputField
           startIcon={<SearchIcon />}
           placeholder="Search for products..."
-          className="min-w-[36rem] bg-shade-200 dark:bg-transparent"
+          className="min-w-[36rem] bg-shade-200 max-lg:hidden"
           iconProps={{
-            className: "stroke-black/40",
+            className: "max-lg:hidden stroke-black/40",
           }}
         />
 
-        <div className="flex gap-8 *:size-10">
+        <div className="flex items-center justify-center gap-4 *:size-6">
+          <div className="lg:hidden">
+            <SearchIcon className="size-6" />
+          </div>
           <ShoppingCart />
           <CircleUserRound />
         </div>
       </nav>
+
+      {/* MOBILE NAVIGATION */}
+      <div
+        className={cn(
+          "full-width content-grid absolute top-full min-h-[calc(100dvh-4rem)] w-full lg:!hidden",
+          !openNav && "!hidden",
+        )}
+      >
+        <div
+          onClick={() => setOpenNav((prev) => !prev)}
+          className="full-width absolute inset-0 size-full bg-shade-100/50 backdrop-blur-sm"
+        />
+
+        <nav className="z-10 h-40 bg-black px-8 py-6 text-white">
+          <ul className="flex flex-col items-center justify-center gap-6">
+            {navlinks.map(({ name, link }) => (
+              <li key={name} className="text-xl">
+                <Link href={link} className="block">
+                  {name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
