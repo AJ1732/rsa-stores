0d59ee5cff6ea004f8cbde9366c1d7fc3b656323
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 17:12:49 +0100

    chore: update

--- src/components/sections/Home/CategorySection/index.tsx
+++ src/components/sections/Home/CategorySection/index.tsx
@@ -3,7 +3,7 @@ const CategorySection = () => {
     <section className="py-20">
       <div className="space-y-[1.75rem] rounded-[1.25rem] bg-shade-200 px-6 py-10 md:space-y-[3rem] md:rounded-3xl md:px-12 md:py-12 lg:space-y-16 lg:rounded-[2.5rem] lg:px-[4.75rem] lg:py-16">
         <h2 className="text-center font-integral text-[2rem] leading-9 md:text-[2.5rem] lg:text-[3rem]">
-          BROWSE BY dress STYLE
+          Browse By categories
         </h2>
 
         <div className="grid size-full gap-5 md:grid-cols-2 lg:grid-cols-3">
