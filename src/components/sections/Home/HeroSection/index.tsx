b1b9bdd0c6ad9949df3d6f79a0f2d47ddaf42a79
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 18:34:14 +0100

    chore: update

--- src/components/sections/Home/HeroSection/index.tsx
+++ src/components/sections/Home/HeroSection/index.tsx
@@ -21,17 +21,17 @@ const HeroSection = () => {
       <div className="flex items-center justify-between gap-20 max-lg:flex-col lg:gap-8">
         <div className="max-w-[38rem] space-y-12">
           <div className="space-y-5 lg:space-y-8">
-            <h2 className="font-integral text-4xl md:text-5xl max-md:max-w-[22.5rem] leading-[2.125rem] text-black lg:text-[4rem] lg:leading-[4rem]">
+            <h2 className="font-integral text-4xl leading-[2.125rem] text-black max-md:max-w-[22.5rem] md:text-5xl lg:text-[4rem] lg:leading-[4rem]">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </h2>
 
-            <p className="max-md:max-w-[22.5rem] text-sm text-black/60 md:text-base">
+            <p className="text-sm text-black/60 max-md:max-w-[22.5rem] md:text-base">
               Browse through our diverse range of meticulously crafted garments,
               designed to bring out your individuality and cater to your sense
               of style.
             </p>
 
-            <Button label="Shop Now" className="px-14 py-4 w-full md:w-fit" />
+            <Button label="Shop Now" className="w-full px-14 py-4 md:w-fit" />
           </div>
 
           <div className="flex flex-wrap items-center justify-center gap-4 space-x-2 md:justify-between">
