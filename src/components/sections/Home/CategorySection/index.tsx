2cce84faa18082fff2cf5f8d0f952c845db471d3
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 18:12:05 +0100

    chore: build category section

--- src/components/sections/Home/CategorySection/index.tsx
+++ src/components/sections/Home/CategorySection/index.tsx
@@ -1,4 +1,9 @@
-const CategorySection = () => {
+import Image from "next/image";
+import { getCategories } from "@/data";
+
+const CategorySection = async () => {
+  const categories = await getCategories();
+
   return (
     <section className="py-20">
       <div className="space-y-[1.75rem] rounded-[1.25rem] bg-shade-200 px-6 py-10 md:space-y-[3rem] md:rounded-3xl md:px-12 md:py-12 lg:space-y-16 lg:rounded-[2.5rem] lg:px-[4.75rem] lg:py-16">
@@ -7,15 +12,24 @@ const CategorySection = () => {
         </h2>
 
         <div className="grid size-full gap-5 md:grid-cols-2 lg:grid-cols-3">
-          {[1, 2, 3, 4, 5].map((item) => (
-            <div
-              key={item}
-              className="relative min-h-72 rounded-[1.25rem] bg-white md:[&:nth-child(1)]:col-span-2 lg:[&:nth-child(1)]:col-span-3 lg:[&:nth-child(2)]:col-span-2 lg:[&:nth-child(5)]:col-span-2"
+          {categories.map(({ id, name, image }) => (
+            <figure
+              key={id}
+              className="group relative min-h-72 overflow-hidden rounded-[1.25rem] bg-white md:[&:nth-child(1)]:col-span-2 lg:[&:nth-child(1)]:col-span-3 lg:[&:nth-child(2)]:col-span-2 lg:[&:nth-child(5)]:col-span-2"
             >
-              <h3 className="absolute left-9 top-6 text-4xl font-bold">
-                {item}
+              <div className="absolute inset-0 z-[1] size-full [border-image:linear-gradient(hsl(220_15%_7%_/_0.5),_hsl(230_12%_12%_/_0.6))_fill_1]" />
+
+              <h3 className="absolute left-9 top-6 z-[1] text-2xl font-bold text-white md:text-3xl lg:text-4xl">
+                {name}
               </h3>
-            </div>
+              <Image
+                src={image}
+                layout="fill"
+                objectFit="cover"
+                alt="Category Image"
+                className="transition-all duration-300 ease-in-out group-hover:scale-105"
+              />
+            </figure>
           ))}
         </div>
       </div>
