3e0ab37eea3a6350ce95bb01245d61369216a9bf
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 16:12:12 +0100

    chore: update

--- src/components/sections/Home/HeroSection/index.tsx
+++ src/components/sections/Home/HeroSection/index.tsx
@@ -20,21 +20,21 @@ const HeroSection = () => {
     <section className="full-width content-grid bg-shade-100 pb-28 pt-24">
       <div className="flex items-center justify-between gap-20 max-lg:flex-col lg:gap-8">
         <div className="max-w-[38rem] space-y-12">
-          <div className="space-y-8">
-            <h2 className="font-integral text-[4rem] leading-[4rem] text-black">
+          <div className="space-y-5 lg:space-y-8">
+            <h2 className="font-integral text-4xl md:text-5xl max-md:max-w-[22.5rem] leading-[2.125rem] text-black lg:text-[4rem] lg:leading-[4rem]">
               FIND CLOTHES THAT MATCHES YOUR STYLE
             </h2>
 
-            <p className="text-black/60">
+            <p className="max-md:max-w-[22.5rem] text-sm text-black/60 md:text-base">
               Browse through our diverse range of meticulously crafted garments,
               designed to bring out your individuality and cater to your sense
               of style.
             </p>
 
-            <Button label="Shop Now" className="px-14 py-4" />
+            <Button label="Shop Now" className="px-14 py-4 w-full md:w-fit" />
           </div>
 
-          <div className="flex items-center justify-between">
+          <div className="flex flex-wrap items-center justify-center gap-4 space-x-2 md:justify-between">
             {stats.map(({ figures, text }, i) => (
               <div key={i}>
                 <p className="text-[2.5rem] font-bold">{figures}+</p>
