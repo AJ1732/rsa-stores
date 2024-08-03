dd6d381cea399332c81a8ffee519c88d253552f2
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 19:30:51 +0100

    refactor: update component

--- src/components/ui-build/Card/ProductCard/index.tsx
+++ src/components/ui-build/Card/ProductCard/index.tsx
@@ -5,14 +5,23 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { ProductCardProps } from "@/types/card";
 import { formatCurrency } from "@/utils/formatCurrency";
 
-const ProductCard: React.FC<ProductCardProps> = ({ title }) => {
-  // 10.75rem;
-  // height: 10.875rem
+const ProductCard: React.FC<ProductCardProps> = ({
+  title,
+  description,
+  className,
+  price,
+}) => {
   return (
-    <Card className="max-w-[10.75rem] border-0 shadow-none *:p-0 md:max-w-[18.5rem]">
+    <Card
+      className={cn(
+        "max-w-[10.75rem] border-0 shadow-none *:p-0 md:max-w-[18.5rem]",
+        className,
+      )}
+    >
       <figure className="mb-4 h-[10.875rem] w-[10.75rem] rounded-[1.25rem] bg-[#F0EEED] md:h-[18.625rem] md:w-[18.4375rem]"></figure>
 
       <CardHeader>
@@ -20,13 +29,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ title }) => {
           {title}
         </CardTitle>
         <CardDescription className="line-clamp-2">
-          Elevate your wardrobe with this stylish black t-shirt featuring a
-          striking monochrome mountain range graphic.
+          {description}
         </CardDescription>
       </CardHeader>
 
       <CardContent className="mt-4 text-xl font-bold md:text-2xl">
-        <p>{formatCurrency(100)}</p>
+        <p>{formatCurrency(price ?? 0)}</p>
       </CardContent>
     </Card>
   );
