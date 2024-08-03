947e12b91412c81fde7fd7e779dd9634f1c8f241
AJ1732 <ejemeniboi@gmail.com>
2024-08-03 19:30:11 +0100

    refactor: update function

--- src/data/getProductsByFilter/index.ts
+++ src/data/getProductsByFilter/index.ts
@@ -1,19 +1,23 @@
-// https://api.escuelajs.co/api/v1/products/?title=Generic&price_min=900&price_max=1000&categoryId=1
+import { ProductProps, ProductFilterProps } from "@/types/data";
 
-import { ProductProps } from "@/types/data";
-
-const getProductsByFilter = async (
-  title: string = "",
-  price: number = 0,
-  price_min: number = 0,
-  price_max: number = 0,
-): Promise<ProductProps[]> => {
+const getProductsByFilter = async ({
+  title = "",
+  price,
+  price_min,
+  price_max,
+  offset,
+  limit,
+  categoryId,
+}: ProductFilterProps = {}): Promise<ProductProps[]> => {
   const params = new URLSearchParams();
 
   if (title) params.append("title", title);
-  if (price !== 0) params.append("price", price.toString());
-  if (price_min !== 0) params.append("price_min", price_min.toString());
-  if (price_max !== 0) params.append("price_max", price_max.toString());
+  if (price !== undefined) params.append("price", price.toString());
+  if (price_min !== undefined) params.append("price_min", price_min.toString());
+  if (price_max !== undefined) params.append("price_max", price_max.toString());
+  if (offset !== undefined) params.append("offset", offset.toString());
+  if (limit !== undefined) params.append("limit", limit.toString());
+  if (categoryId !== undefined) params.append("categoryId", categoryId.toString());
 
   const url = `https://api.escuelajs.co/api/v1/products/?${params.toString()}`;
 
