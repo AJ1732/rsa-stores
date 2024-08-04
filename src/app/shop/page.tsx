0b2fbaa98c1f5143f2f119733d43357a4ba26e6a
AJ1732 <ejemeniboi@gmail.com>
2024-08-05 00:42:22 +0100

    chore: update

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,20 @@
 import { ProductCard } from "@/components";
+import { getProducts, getProductsByFilter } from "@/data";
 
-const ShopPage = () => {
+const ShopPage = async () => {
+  const products = await getProductsByFilter({ offset: 0, limit: 9 });
   return (
-    <section className="pt-12 pb-16">
-      <div className="flex gap-5 overflow-x-auto">
-        <ProductCard id={1} />
-        <ProductCard id={1} />
+    <section className="pb-20 pt-12">
+      <div className="grid grid-cols-4 gap-5">
+        <div className="max-w-[18.5rem] bg-red-500">Filter</div>
+
+        <div className="col-span-3 grid grid-cols-subgrid gap-y-5">
+          {products.map((product) => (
+            <ProductCard key={product.id} id={product.id} />
+          ))}
+
+          <div className="col-span-3 text-center md:col-span-2">Pagination</div>
+        </div>
       </div>
     </section>
   );
