4a9647c87a2eef80d5df350053e2ccedab20378c
AJ1732 <ejemeniboi@gmail.com>
2024-08-04 19:31:26 +0100

    chore: update

---
+++ src/app/shop/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { ProductCard } from "@/components";
+
+const ShopItemPage = () => {
+  return (
+    <div>
+      <ProductCard id={1} title="T-Shirt with Tape Details" />
+    </div>
+  );
+};
+export default ShopItemPage;
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,13 +1,13 @@
 import { ProductCard } from "@/components";
 
-const ProductsPage = () => {
+const ShopPage = () => {
   return (
     <section>
-      <div className="flex gap-5 py-20 overflow-x-auto">
+      <div className="flex gap-5 overflow-x-auto py-20">
         <ProductCard id={1} title="T-Shirt with Tape Details" />
         <ProductCard id={1} title="T-Shirt with Tape Details" />
       </div>
     </section>
   );
 };
-export default ProductsPage;
+export default ShopPage;
